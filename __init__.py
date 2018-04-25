346d5e19d41d8347a2b6648cd45846b93fbd35e2
Miquel Perelló Nieto <perello.nieto@gmail.com>
2018-04-25 18:37:58 +0100

    Fixed scores - Avoid 0 and 1 scores by applying an epsilon difference on them - Apply log function during prediction and predict_proba

--- __init__.py
+++ __init__.py
@@ -9,22 +9,25 @@ class DirichletCalibrator(BaseEstimator, RegressorMixin):
     def __init__(self):
             self.calibrator_ = None
 
-    def fit(self, X, y, sample_weight=None):
+    def fit(self, X, y, *args, **kwargs):
         n = len(y)
+
+        eps = np.finfo(X.dtype).eps
+        X = np.log(np.clip(X, eps, 1-eps))
 
         self.calibrator_ = LogisticRegression(
             C=99999999999,
             multi_class='multinomial', solver='saga'
-            ).fit(
-                np.log(X), 
-                y, 
-                sample_weight
-                )
+            ).fit( X, y, *args, **kwargs)
 
         return self
 
     def predict_proba(self, S):
+        eps = np.finfo(S.dtype).eps
+        S = np.log(np.clip(S, eps, 1-eps))
         return self.calibrator_.predict_proba(S)
 
     def predict(self, S):
+        eps = np.finfo(S.dtype).eps
+        S = np.log(np.clip(S, eps, 1-eps))
         return self.calibrator_.predict(S)
