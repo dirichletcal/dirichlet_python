new implementation of the diag and fix-diag approches with analytic gradient and hessian.

calib/fixeddirichlet.py
@@ -5,18 +5,41 @@ from .fulldirichlet import FullDirichletCalibrator
 
 
 class FixedDiagonalDirichletCalibrator(FullDirichletCalibrator):
+    
     def fit(self, X, y, *args, **kwargs):
+        
         eps = np.finfo(X.dtype).eps
         X_ = np.log(np.clip(X, eps, 1-eps))
-
-        k = len(np.unique(y))
-
-        weights_0 = np.append(np.random.randn(k-1), np.random.rand())
-        bounds = [(-np.inf, np.inf) for _ in range(k-1)]
-        bounds.append((0, np.inf))
-
-        self.calibrator_ = MultinomialRegression(
-            weights_0=weights_0, bounds=bounds).fit(X_, y, *args, **kwargs)
+        k = np.shape(X_)[1]
+        for i in range(0, k-1):
+            X_[:, i] = X[:, i] - X[:, -1]
+        X_ = X_[:, :-1]
+        self.calibrator_ = MultinomialRegression(method='FixDiag').fit(X_, y, *args, **kwargs)
         self.coef_ = self.calibrator_.coef_
         self.intercept_ = self.calibrator_.intercept_
+
         return self
+
+    def predict_proba(self, S):
+        
+        eps = np.finfo(S.dtype).eps
+        S = np.log(np.clip(S, eps, 1-eps))
+        k = np.shape(S)[1]
+        for i in range(0, k-1):
+            S[:, i] = S[:, i] - S[:, -1]
+        S = S[:, :-1]
+
+        return self.calibrator_.predict_proba(S)
+
+    def predict(self, S):
+        
+        eps = np.finfo(S.dtype).eps
+        S = np.log(np.clip(S, eps, 1-eps))
+        k = np.shape(S)[1]
+        for i in range(0, k-1):
+            S[:, i] = S[:, i] - S[:, -1]
+        S = S[:, :-1]
+
+        return self.calibrator_.predict(S)
+
+

calib/fulldirichlet.py
@@ -11,7 +11,7 @@ class FullDirichletCalibrator(BaseEstimator, RegressorMixin):
     def fit(self, X, y, *args, **kwargs):
         eps = np.finfo(X.dtype).eps
         X_ = np.log(np.clip(X, eps, 1-eps))
-        self.calibrator_ = MultinomialRegression().fit(X_, y, *args, **kwargs)
+        self.calibrator_ = MultinomialRegression(method='Full').fit(X_, y, *args, **kwargs)
         self.coef_ = self.calibrator_.coef_
         self.intercept_ = self.calibrator_.intercept_
 

calib/multinomial.py
@@ -10,7 +10,6 @@ from scipy.optimize import minimize
 import scipy
 import scipy.optimize
 
-
 def new_minimize_trust_region(fun, x0, args=(), jac=None, hess=None, hessp=None,
                            subproblem=None, initial_trust_radius=1.0,
                            max_trust_radius=1000.0, eta=0.15, gtol=1e-4,
@@ -183,66 +182,94 @@ scipy.optimize._trustregion._minimize_trust_region.__code__ = new_minimize_trust
 
 
 class MultinomialRegression(BaseEstimator, RegressorMixin):
-    def __init__(self, weights_0=None, bounds=None):
+    def __init__(self, weights_0=None, method='Full'):
+        # Method in {'Full', 'Diag', 'FixDiag'}
         self.coef_ = None
         self.intercept_ = None
         self.weights_0_ = weights_0
-        self.bounds_ = None
+        self.method_ = method
 
     def fit(self, X, y, *args, **kwargs):
 
         X_ = np.hstack((X, np.ones((len(X), 1))))
 
         classes = np.unique(y)
+
         k = len(classes)
+
         target = label_binarize(y, classes)
+
         if k == 2:
             target = np.hstack([target, 1-target])
 
         if self.weights_0_ is None:
-            weights_0 = np.random.randn(k + 1, k - 1)
-            weights_0[np.diag_indices(k - 1)] = np.abs(weights_0[np.diag_indices(k - 1)])
-            weights_0[k - 1] = np.abs(weights_0[k - 1]) * -1
+            
+            if self.method_ is 'Full':
+
+                weights_0 = np.random.randn((k - 1) * (k + 1))
+
+                weights_0 = _get_weights(weights_0, k, self.method_)
+
+                weights_0[np.diag_indices(k - 1)] = np.abs(weights_0[np.diag_indices(k - 1)])
+                
+                weights_0[:, k - 1] = np.abs(weights_0[:, k - 1]) * -1
+
+            elif self.method_ is 'Diag':
+
+                weights_0 = np.random.randn(k + (k - 1))
+
+                weights_0 = _get_weights(weights_0, k, self.method_)
+
+                weights_0[np.diag_indices(k)] = np.abs(weights_0[np.diag_indices(k)])
+
+            elif self.method_ is 'FixDiag':
+
+                weights_0 = np.random.randn(k)
+
+                weights_0[0] = np.abs(weights_0[0])
+
         else:
             weights_0 = self.weights_0_
 
-        weights_0 = weights_0.transpose().ravel()
+        weights_0 = weights_0[weights_0!=0]
 
-        # if self.bounds_ is None:
-        #    dims = (k+1, k-1)
-        #    diag_ravel_ind = np.ravel_multi_index(np.diag_indices(k-1), dims)
+        n = np.shape(X_)[0]
 
-        #    k_ind = [np.ones(k-1, dtype=int)*(k-1), np.arange(k-1)]
-        #    k_ravel_ind = np.ravel_multi_index(k_ind, dims)
+        m = np.shape(X_)[1]
 
-        #    bounds = []
-        #    for ind, _ in enumerate(weights_0):
-        #        if ind in diag_ravel_ind:
-        #            bounds.append((0, np.inf))
-        #        elif ind in k_ravel_ind:
-        #            bounds.append((-np.inf, 0))
-        #        else:
-        #            bounds.append((-np.inf, np.inf))
-        # else:
-        #    bounds = self.bounds_
+        XXT = np.zeros([n, m, m])
+
+        for i in range(0, n):
+            XXT[i, :, :] = np.matmul(X_[i, :].reshape(-1, 1), X_[i, :].reshape(-1, 1).transpose())
+
+
+        print(self.method_)
 
         res = minimize(
-            method='trust-exact',
-            fun=_objective,
-            jac=_gradient,
-            hess=_hessian,
-            x0=weights_0,
-            args=(X_, target, k),
-            bounds=None,
-            #tol=1e-16,
-            options={'disp': False,
-                     'initial_trust_radius': 1.0,
-                     'max_trust_radius': 1e32,
-                     'change_ratio': 1 - 1e-3,
-                     'eta': 0.0,
-                     'maxiter': 1e4,
-                     'gtol': 1e-8}
-        )
+                method='trust-exact',
+                fun=_objective,
+                jac=_gradient,
+                hess=_hessian,
+                x0=weights_0,
+                args=(X_, XXT, target, k, self.method_),
+                bounds=None,
+                #tol=1e-16,
+                options={'disp': False,
+                    'initial_trust_radius': 1.0,
+                    'max_trust_radius': 1e32,
+                    'change_ratio': 1 - 1e-3,
+                    'eta': 0.0,
+                    'maxiter': 1e4,
+                    'gtol': 1e-8}
+                )
+        
+        #res = minimize(
+        #    method='BFGS',
+        #    fun=_objective,
+        #    jac=_gradient,
+        #    x0=weights_0,
+        #    args=(X_, XXT, target, k, self.method_),
+        #)
 
         weights = res.x
 
@@ -254,18 +281,18 @@ class MultinomialRegression(BaseEstimator, RegressorMixin):
 
         np.set_printoptions(precision=3)
         print('gradient is:')
-        print(_gradient(weights, X_, target, k).reshape(-1, k+1).transpose())
+        print(_gradient(weights, X_, XXT, target, k, self.method_))
         print('mean target is:')
         print(np.mean(target, axis=0))
         print('mean output is:')
-        print(np.mean(_calculate_outputs(_get_weights(weights, k), X_), axis=0))
+        print(np.mean(_calculate_outputs(_get_weights(weights, k, self.method_), X_), axis=0))
         print('reason for termination:')
         print(res.message)
         print('===================================================================')
 
-        self.weights_ = _get_weights(weights, k)
-        self.coef_ = self.weights_.transpose()[:, :-1]
-        self.intercept_ = self.weights_.transpose()[:, -1]
+        self.weights_ = _get_weights(weights, k, self.method_)
+        self.coef_ = self.weights_[:, :-1]
+        self.intercept_ = self.weights_[:, -1]
         return self
 
     def predict_proba(self, S):
@@ -276,23 +303,40 @@ class MultinomialRegression(BaseEstimator, RegressorMixin):
         return self.predict_proba(S)
 
 
-def _get_weights(params, k):
-    n_params = len(params)
-    if n_params == k ** 2 - 1:
-        return params.reshape(-1, k + 1).transpose()
-    else:
-        value = params[-1]
-        intercepts = params[:-1]
-        weights = np.zeros((k + 1, k - 1))
-        weights[np.diag_indices(k - 1)] = value
-        weights[k - 1] = value * -1
-        weights[k] = intercepts
-        return weights
+def _get_weights(params, k, method):
+
+    if method is 'Full':
+
+        weights = np.zeros([k, k+1])
+        
+        weights[:-1, :] = params.reshape(-1, k + 1)
+
+    elif method is 'Diag':
+        
+        weights = np.zeros([k, k+1])
+
+        tmp_params = params[:-1].reshape(-1, 2)
+
+        weights[np.diag_indices(k - 1)] = tmp_params[:, 0]
+
+        weights[:-1, -1] = tmp_params[:, 1]
+
+        weights[-1, k] = params[-1]
+
+    elif method is 'FixDiag':
+        
+        weights = np.zeros([k, k])
+
+        weights[np.diag_indices(k)] = params[0]
+
+        weights[:-1, -1] = params[1:]
+
+    return weights
 
 
 def _objective(params, *args):
-    (X, y, k) = args
-    weights = _get_weights(params, k)
+    (X, _, y, k, method) = args
+    weights = _get_weights(params, k, method)
     outputs = _calculate_outputs(weights, X)
     loss = log_loss(y, outputs)
     #print('Loss is:')
@@ -303,33 +347,106 @@ def _objective(params, *args):
 
 
 def _gradient(params, *args):
-    (X, y, k) = args
-    weights = _get_weights(params, k)
+    (X, _, y, k, method) = args
+    weights = _get_weights(params, k, method)
     outputs = _calculate_outputs(weights, X)
-    graident = np.zeros((k + 1, k - 1))
-    for i in range(0, k - 1):
-        graident[:, i] = np.sum((outputs[:, i] - y[:, i]).reshape(-1, 1).repeat(k+1, axis=1) * X, axis=0)
+
+    if method is 'Full':
+
+        gradient = np.random.randn((k - 1) * (k + 1))
+
+        for i in range(0, k - 1):
+
+            gradient[i * (k + 1):(i + 1) * (k + 1)] = \
+                    np.sum((outputs[:, i] - y[:, i]).reshape(-1, 1).repeat(k+1, axis=1) * X, axis=0)
+
+    elif method is 'Diag':
+
+        gradient = np.random.randn(k + (k - 1))
+
+        for i in range(0, k - 1):
+
+            gradient[i * 2:(i + 1) * 2] = \
+                    np.sum((outputs[:, i] - y[:, i]).reshape(-1, 1).repeat(2, axis=1) * X[:, [i, k]], axis=0)
+
+        gradient[-1] = np.sum((outputs[:, i] - y[:, i]) * X[:, k])
+
+    elif method is 'FixDiag':
+        
+        gradient = np.random.randn(k)
+
+        gradient[0] = np.sum((outputs[:, :-1] - y[:, :-1]) * X[:, :-1])
+
+        for i in range(0, k - 1):
+
+            gradient[i + 1] = np.sum((outputs[:, i] - y[:, i]) * X[:, k - 1])
+
     #print(graident)
-    return graident.transpose().ravel()
+
+    return gradient
 
 
 def _hessian(params, *args):
-    (X, y, k) = args
-    weights = _get_weights(params, k)
+    (X, XXT, y, k, method) = args
+    weights = _get_weights(params, k, method)
     outputs = _calculate_outputs(weights, X)
-    hessian = np.zeros((k**2 - 1, k**2 - 1))
     n = np.shape(X)[0]
-    XXT = np.zeros((n, k+1, k+1))
-    for i in range(0, n):
-        XXT[i, :, :] = np.matmul(X[i, :].reshape(-1, 1), X[i, :].reshape(-1, 1).transpose())
-    for i in range(0, k-1):
-        for j in range(0, k-1):
-            if i <= j:
-                tmp_diff = outputs[:, i] * (int(i == j) - outputs[:, j])
-                tmp_diff = tmp_diff.ravel().repeat((k+1)**2).reshape(n, k+1, k+1)
-                hessian[i*(k+1):(i+1)*(k+1), j*(k+1):(j+1)*(k+1)] = np.sum(tmp_diff * XXT, axis=0)
-            else:
-                hessian[i*(k+1):(i+1)*(k+1), j*(k+1):(j+1)*(k+1)] = hessian[j*(k+1):(j+1)*(k+1), i*(k+1):(i+1)*(k+1)]
+
+    if method is 'Full':
+
+        hessian = np.zeros([k ** 2 - 1, k ** 2 - 1])
+
+        for i in range(0, k - 1):
+            for j in range(0, k - 1):
+                if i <= j:
+                    tmp_diff = outputs[:, i] * (int(i == j) - outputs[:, j])
+                    tmp_diff = tmp_diff.ravel().repeat((k + 1) ** 2).reshape(n, k + 1, k + 1)
+                    hessian[i * (k + 1): (i + 1) * (k + 1), j * (k + 1): (j + 1) * (k + 1)] = np.sum(tmp_diff * XXT, axis=0)
+                else:
+                    hessian[i * (k + 1): (i + 1) * (k + 1), j * (k + 1): (j + 1) * (k + 1)] = \
+                            hessian[j * (k + 1): (j + 1) * (k + 1), i * (k + 1): (i + 1) * (k + 1)]
+
+    elif method is 'Diag':
+
+        hessian = np.zeros([2*k - 1, 2*k - 1])
+
+        for i in range(0, k - 1):
+            for j in range(0, k - 1):
+                if i <= j:
+                    sub_XXT_1 = XXT[:, [i, i, k, k], [i, k, i, k]].reshape(-1, 2, 2)
+                    sub_XXT_2 = XXT[:, [j, j, k, k], [i, k, i, k]].reshape(-1, 2, 2)
+
+                    tmp_product = (outputs[:, i] * outputs[:, j]).ravel().repeat(4).reshape(n, 2, 2)
+
+                    if i == j:
+                        tmp_mu = outputs[:, i].ravel().repeat(4).reshape(n, 2, 2)
+                        hessian[i * 2: (i + 1) * 2, j * 2: (j + 1) * 2] = np.sum(tmp_mu * sub_XXT_1 - tmp_product * sub_XXT_2, axis=0)
+                    else:
+                        hessian[i * 2: (i + 1) * 2, j * 2: (j + 1) * 2] = np.sum(-tmp_product * sub_XXT_2, axis=0)
+                else:
+                        hessian[i * 2: (i + 1) * 2, j * 2: (j + 1) * 2] = hessian[j * 2: (j + 1) * 2, i * 2: (i + 1) * 2]
+
+    elif method is 'FixDiag':
+
+        hessian = np.zeros([k, k])
+
+        for i in range(0, k - 1):
+            for j in range(0, k - 1):
+                if i <= j:
+                    tmp_diff = outputs[:, i] * (int(i == j) - outputs[:, j])
+                    hessian[i + 1, j + 1] = np.sum(tmp_diff * XXT[:, k - 1, k - 1], axis=0)
+                else:
+                    hessian[i + 1, j + 1] = hessian[j + 1, i + 1]
+
+        tmp_product = np.sum(outputs[:, :-1] * X[:, :-1], axis=1).reshape(-1, 1).repeat(k - 1, 1) * outputs[:, :-1] * X[:, :-1]
+
+        hessian[0, 0] = np.sum(np.sum(outputs[:, :-1] * (X[:, :-1] ** 2), axis=1) - np.sum(tmp_product, axis=1))
+        
+        tmp_product = np.sum(outputs[:, :-1] * X[:, :-1], axis=1).reshape(-1, 1).repeat(k - 1, 1) * outputs[:, :-1] * X[:, -1].reshape(-1, 1).repeat(k - 1, 1)
+        
+        for i in range(0, k - 1):
+            hessian[0, i + 1] = np.sum(outputs[:, i] * X[:, i] * X[:, -1] - tmp_product[:, i])
+            hessian[i + 1, 0] = hessian[0, i + 1]
 
     #np.set_printoptions(precision=1)
     #print('hessian is:')
@@ -340,9 +457,11 @@ def _hessian(params, *args):
 
 
 def _calculate_outputs(weights, X):
-    k = len(weights) - 1
-    mul = np.zeros((len(X), k))
-    mul[:, :k - 1] = np.dot(X, weights)
+    
+    k = np.shape(weights)[0]
+    
+    mul = np.dot(X, weights.transpose())
+    
     return _softmax(mul)
 
 