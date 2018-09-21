388911317f34caa6b11e26f7693e4ab07817ff04
Miquel Perelló Nieto <perello.nieto@gmail.com>
2018-09-21 15:36:31 +0100

    Added test for Multinomial

--- tests/calib/test_multinomial.py
+++ tests/calib/test_multinomial.py
@@ -1,19 +1,44 @@
 import unittest
 import numpy as np
 from dirichlet.calib.multinomial import MultinomialRegression
+from dirichlet.calib.multinomial import _get_weights
+from . import get_simple_binary_example
+from . import get_simple_ternary_example
 
 
 class TestMultinomial(unittest.TestCase):
-    def setUp(self):
+    def test_fit_predict(self):
+        S, y = get_simple_binary_example()
         self.mlr = MultinomialRegression()
-
-    def test_fit(self):
-        S = np.array([[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]]).T
-        S = np.hstack((np.flip(S, axis=0), S))
-        y = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
         self.mlr.fit(S, y)
         predictions = self.mlr.predict_proba(S).argmax(axis=1)
         self.assertTrue(np.alltrue(np.equal(predictions, y)))
+
+        S, y = get_simple_ternary_example()
+        self.mlr = MultinomialRegression()
+        self.mlr.fit(S, y)
+        predictions = self.mlr.predict_proba(S).argmax(axis=1)
+        np.testing.assert_array_equal(predictions, y)
+
+
+    def test_get_weights(self):
+        k = 3
+        params = np.arange((k-1)*(k+1)) + 1
+        full_matrix = _get_weights(params, k=k, method='Full')
+        expected = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [0, 0, 0, 0]], 'float')
+        np.testing.assert_array_equal(full_matrix, expected)
+
+        k = 3
+        params = np.arange(k + k - 1) + 1
+        full_matrix = _get_weights(params, k=k, method='Diag')
+        expected = np.array([[1, 0, 0, 2], [0, 3, 0, 4], [0, 0, 5, 0]], 'float')
+        np.testing.assert_array_equal(full_matrix, expected)
+
+        k = 3
+        params = np.arange(k) + 1
+        full_matrix = _get_weights(params, k=k, method='FixDiag')
+        expected = np.array([[1, 0, 2], [0, 1, 3], [0, 0, 0]], 'float')
+        np.testing.assert_array_equal(full_matrix, expected)
 
 
 def main():
