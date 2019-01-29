e46d85ead1bac4fefe07edc6cc78c93d0839e7e5
Hao Song <nuacesh@gmail.com>
2019-01-29 17:20:53 +0000

    switched to tensorflow

--- calib/typeiidirichlet.py
+++ calib/typeiidirichlet.py
@@ -1,7 +1,14 @@
-import autograd
-import autograd.numpy as numpy
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+
+import tensorflow.contrib.eager as tfe
+
+import numpy
 
 from sklearn.base import BaseEstimator, RegressorMixin
+
+tf.enable_eager_execution()
 
 
 class TypeIIDirichletCalibrator(BaseEstimator, RegressorMixin):
@@ -12,8 +19,8 @@ class TypeIIDirichletCalibrator(BaseEstimator, RegressorMixin):
 
         self.theta_w = None
 
-    def fit(self, x, y, batch_size=16, sample_size=128, lr=1e-2, beta_1=0.9, beta_2=0.999,
-            eps=1e-8, max_batch=int(1e8), factr=0.0, **kwargs):
+    def fit(self, x, y, lr=0.05, batch_size=1024,
+            sample_size=128, ftol=1e-4, max_batch=int(1e8), **kwargs):
 
         n_y = numpy.shape(x)[0]
 
@@ -35,133 +42,79 @@ class TypeIIDirichletCalibrator(BaseEstimator, RegressorMixin):
             y_hot[y == i, i] = 1.0
             theta_w[i, i] = 1.0
 
-        m = numpy.zeros_like(theta_w)
+        fin_theta_w = theta_w.copy()
 
-        v = numpy.zeros_like(theta_w)
+        theta_w = tf.Variable(theta_w, 'Theta')
 
-        fin_theta_w = theta_w
+        get_obj_g = tfe.gradients_function(self._objective, params=[0])
 
-        get_gradient = autograd.elementwise_grad(self._objective, 0)
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
 
-        batch_idx = numpy.arange(0, n_y, batch_size)
+        D = tf.data.Dataset.from_tensor_slices((tf.cast(feature_x, tf.float64), tf.cast(y_hot, tf.float64)))
 
-        if len(batch_idx) == 1:
-            feature_x = feature_x.repeat(batch_size, axis=0)
-            y_hot = y_hot.repeat(batch_size, axis=0)
-            n_y = numpy.shape(y_hot)[0]
-            per_idx = numpy.random.permutation(n_y)
-            feature_x = feature_x[per_idx]
-            y_hot = y_hot[per_idx]
-            batch_idx = numpy.arange(0, n_y, batch_size)
-
-        batch_num = len(batch_idx) - 1
-
-        converge = False
+        D = D.shuffle(1024).batch(batch_size)
 
         batch_L = []
 
         mean_L = []
 
-        for i in range(0, int(1e8)):
+        for (batch, (f_x, y_h)) in enumerate(D.repeat(1e8)):
 
-            for j in range(0, batch_num):
+            L_t = self._objective(theta_w, f_x, y_h, k, sample_size)
 
-                L_t = self._objective(theta_w,
-                                      feature_x[batch_idx[j]:batch_idx[j+1]],
-                                      y_hot[batch_idx[j]:batch_idx[j+1]],
-                                      k,
-                                      sample_size)
+            batch_L.append(L_t)
 
-                # print(L_t)
+            g_t = tf.reduce_mean(get_obj_g(theta_w, f_x, y_h, k, sample_size), axis=0)
 
-                g_t = get_gradient(theta_w,
-                                   feature_x[batch_idx[j]:batch_idx[j+1]],
-                                   y_hot[batch_idx[j]:batch_idx[j+1]],
-                                   k, sample_size)
+            optimizer.apply_gradients([(g_t, theta_w)],
+                                      global_step=tf.train.get_or_create_global_step())
 
-                # print(L_t)
+            if len(batch_L) >= 8:
+                mean_L.append(numpy.mean(batch_L[-8:]))
+            else:
+                for o in range(0, len(mean_L)):
+                    mean_L[o] = numpy.mean(batch_L)
+                mean_L.append(numpy.mean(batch_L))
 
-                g_t[numpy.isnan(g_t)] = 0.0
+            if len(mean_L) >= 2:
+                if mean_L[-1] < numpy.min(mean_L[:-1]):
+                    fin_theta_w = theta_w.numpy().copy()
 
-                g_t[numpy.isinf(g_t)] = 0.0
+            if len(mean_L) > 128:
 
-                m = beta_1 * m + (1 - beta_1) * g_t
+                previous_opt = numpy.min(mean_L.copy()[:-128])
 
-                v = beta_2 * v + (1 - beta_2) * g_t * g_t
+                current_opt = numpy.min(mean_L.copy()[-128:])
 
-                theta_w = theta_w - lr * m / (v ** 0.5 + eps)
+                if previous_opt - current_opt <= ftol:
+                    print(previous_opt, current_opt)
+                    break
 
-                batch_L.append(L_t)
-
-                if len(batch_L) >= 128:
-                    mean_L.append(numpy.mean(batch_L[-128:]))
-                else:
-                    for o in range(0, len(mean_L)):
-                        mean_L[o] = numpy.mean(batch_L)
-                    mean_L.append(numpy.mean(batch_L))
-
-                if len(mean_L) >= 2:
-                    if mean_L[-1] < numpy.min(mean_L[:-1]):
-                        fin_theta_w = theta_w.copy()
-
-                if len(mean_L) > 1024:
-
-                    previous_opt = numpy.min(mean_L.copy()[:-1024])
-
-                    current_opt = numpy.min(mean_L.copy()[-1024:])
-
-                    if previous_opt - current_opt <= numpy.abs(previous_opt * factr):
-
-                        converge = True
-
-                        break
-
-                    if len(mean_L) >= max_batch:
-
-                        converge = True
-
-                        break
-
-            per_idx = numpy.random.permutation(n_y)
-
-            feature_x = feature_x[per_idx]
-
-            y_hot = y_hot[per_idx]
-
-            if converge:
-
-                break
+                if len(mean_L) >= max_batch:
+                    break
 
         self.theta_w = fin_theta_w
-
-        # print(mean_L[-1])
-        #
-        # print('Done!===========================================================================')
 
         return self
 
     def _objective(self, theta_w, feature_x, y_hot, k, sample_size):
 
-        mu_w = theta_w[:k, :].transpose().ravel()
+        mu_w = tf.reshape(tf.transpose(theta_w[:k, :]), [1, -1])
 
-        sigma_w = theta_w[k:, :].transpose().ravel()
+        sigma_w = tf.reshape(tf.transpose(theta_w[k:, :]), [1, -1])
 
-        e = numpy.random.randn(sample_size, k*(k+1))
+        e = tf.random.normal((sample_size, k*(k+1)), dtype=tf.float64)
 
-        sample_w = (e * ((sigma_w**2)**0.5) + mu_w).reshape(sample_size, k+1, k)
+        sample_w = tf.reshape((e * ((sigma_w**2)**0.5) + mu_w), [sample_size, k+1, k])
 
-        raw_prod = numpy.matmul(feature_x, sample_w)
+        raw_prod = tf.tensordot(feature_x, sample_w, axes=[[1], [1]])
 
-        prod = raw_prod - numpy.max(raw_prod, axis=2)[:, :, numpy.newaxis]
+        prod = raw_prod - tf.expand_dims(tf.reduce_max(raw_prod, axis=2), axis=2)
 
-        p_y = numpy.mean(numpy.exp(prod) /
-                         numpy.sum(numpy.exp(prod), axis=2)[:, :, numpy.newaxis].repeat(k, axis=2),
-                         axis=0)
+        p_y = tf.reduce_mean(tf.exp(prod) / tf.expand_dims(tf.reduce_sum(tf.exp(prod), axis=2), 2),
+                             axis=1)
 
-        brier = numpy.mean(numpy.sum((p_y - y_hot)**2, axis=1))
-
-        if numpy.isnan(brier):
-            import pdb; pdb.set_trace()
+        brier = tf.reduce_mean(tf.reduce_sum((p_y - y_hot)**2, axis=1))
 
         return brier
 
@@ -179,19 +132,18 @@ class TypeIIDirichletCalibrator(BaseEstimator, RegressorMixin):
 
         sigma_w = self.theta_w[k:, :].transpose().ravel()
 
-        e = numpy.random.randn(sample_size, k*(k+1))
+        e = tf.random.normal((sample_size, k * (k + 1)), dtype=tf.float64)
 
-        sample_w = (e * ((sigma_w**2)**0.5) + mu_w).reshape(sample_size, k+1, k)
+        sample_w = tf.reshape((e * ((sigma_w ** 2) ** 0.5) + mu_w), [sample_size, k + 1, k])
 
-        raw_prod = numpy.matmul(feature_x, sample_w)
+        raw_prod = tf.tensordot(feature_x, sample_w, axes=[[1], [1]])
 
-        prod = raw_prod - numpy.max(raw_prod, axis=2)[:, :, numpy.newaxis]
+        prod = raw_prod - tf.expand_dims(tf.reduce_max(raw_prod, axis=2), axis=2)
 
-        p_y = numpy.mean(numpy.exp(prod) /
-                         numpy.sum(numpy.exp(prod), axis=2)[:, :, numpy.newaxis].repeat(k, axis=2),
-                         axis=0)
+        p_y = tf.reduce_mean(tf.exp(prod) / tf.expand_dims(tf.reduce_sum(tf.exp(prod), axis=2), 2),
+                             axis=1)
 
-        return p_y
+        return p_y.numpy()
 
     def predict(self, x):
 
@@ -222,15 +174,4 @@ if __name__ == '__main__':
     calibrator = TypeIIDirichletCalibrator()
     calibrator.fit(S, y)
     print(calibrator.predict_proba(S))
-    
-
-
-
-
-
-
-
-
-
-
 
