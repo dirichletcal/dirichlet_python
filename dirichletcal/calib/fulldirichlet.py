1fff26daf924b580c859dea3ff34c01526ac455f
Miquel Perello-Nieto <perello.nieto@gmail.com>
2020-07-16 11:58:40 +0100

    Simplified fulldirichlet - Removed crossvalidation to find optimal hyperparameters from the model

--- dirichletcal/calib/fulldirichlet.py
+++ dirichletcal/calib/fulldirichlet.py
@@ -10,27 +10,26 @@ from .multinomial import _get_identity_weights
 
 
 class FullDirichletCalibrator(BaseEstimator, RegressorMixin):
-    def __init__(self, l2=0.0, reg_lambda_list=[0.0], reg_mu_list=[None],
+    def __init__(self, l2=0.0, reg_lambda=0.0, reg_mu=None,
                  weights_init=None, initializer='identity', reg_norm=False,
                  ref_row=True):
-    
+
         """
         Params:
             weights_init: (nd.array) weights used for initialisation, if None then idendity matrix used. Shape = (n_classes - 1, n_classes + 1)
             l2: (float) regularization parameter (lambda)
             comp_l2: (bool) If true, then complementary L2 regularization used (off-diagonal regularization)
         """
-    
+
         self.calibrator_ = None
         self.weights_ = weights_init  # Input weights for initialisation
-        self.reg_lambda = 0.0
         self.reg_mu = None
-        self.reg_lambda_list = reg_lambda_list
-        self.reg_mu_list = reg_mu_list  # Complementary L2 regularization. (Off-diagonal regularization)
+        self.reg_lambda = reg_lambda
+        self.reg_mu = reg_mu  # Complementary L2 regularization. (Off-diagonal regularization)
         self.initializer = initializer
         self.reg_norm = reg_norm
         self.ref_row = ref_row
-        
+
     def fit(self, X, y, X_val=None, y_val=None, *args, **kwargs):
 
         k = np.shape(X)[1]
@@ -44,33 +43,21 @@ class FullDirichletCalibrator(BaseEstimator, RegressorMixin):
         _X_val = np.copy(X_val)
         _X_val = np.log(clip_for_log(X_val))
 
-        for i in range(0, len(self.reg_lambda_list)):
-            for j in range(0, len(self.reg_mu_list)):
-                tmp_cal = MultinomialRegression(method='Full', 
-                                                reg_lambda=self.reg_lambda_list[i],
-                                                reg_mu=self.reg_mu_list[j],
-                                                reg_norm=self.reg_norm,
-                                                ref_row=self.ref_row)
-                tmp_cal.fit(_X, y, *args, **kwargs)
-                tmp_loss = log_loss(y_val, tmp_cal.predict_proba(_X_val))
-                
-                if (i + j) == 0:
-                    final_cal = tmp_cal
-                    final_loss = tmp_loss
-                    final_reg_lambda = self.reg_lambda_list[i]
-                    final_reg_mu = self.reg_mu_list[j]
-                elif tmp_loss < final_loss:
-                    final_cal = tmp_cal
-                    final_loss = tmp_loss
-                    final_reg_lambda = self.reg_lambda_list[i]
-                    final_reg_mu = self.reg_mu_list[j]
-
-        self.calibrator_ = final_cal
-        self.reg_lambda = final_reg_lambda
-        self.reg_mu = final_reg_mu
-        self.weights_ = self.calibrator_.weights_
+        self.calibrator_ = MultinomialRegression(method='Full',
+                                        reg_lambda=self.reg_lambda,
+                                        reg_mu=self.reg_mu,
+                                        reg_norm=self.reg_norm,
+                                        ref_row=self.ref_row)
+        self.calibrator_.fit(_X, y, *args, **kwargs)
+        final_loss = log_loss(y_val, self.calibrator_.predict_proba(_X_val))
 
         return self
+
+    @property
+    def weights(self):
+        if self.calibrator_ is not None:
+            return self.calibrator_.weights_
+        return self.weights_init
 
     @property
     def coef_(self):
@@ -87,4 +74,3 @@ class FullDirichletCalibrator(BaseEstimator, RegressorMixin):
     def predict(self, S):
         S = np.log(clip_for_log(S))
         return np.asarray(self.calibrator_.predict(S))
-        
--- dirichletcal/tests/calib/test_fulldirichlet.py
+++ dirichletcal/tests/calib/test_fulldirichlet.py
@@ -1,5 +1,4 @@
 import unittest
-import numpy as np
 from dirichletcal.calib.fulldirichlet import FullDirichletCalibrator
 from . import get_simple_binary_example
 from . import get_extreme_binary_example
@@ -13,19 +12,14 @@ class TestFullDirichlet(unittest.TestCase):
         self.cal = FullDirichletCalibrator()
 
     def test_fit_predict(self):
-        S, y = get_simple_binary_example()
-        self.cal = FullDirichletCalibrator()
-        self.cal.fit(S, y)
-        predictions = self.cal.predict_proba(S).argmax(axis=1)
-        acc = accuracy_score(y, predictions)
-        self.assertGreater(acc, 0.99, "accuracy must be superior to 99 percent")
-
-        S, y = get_simple_ternary_example()
-        self.cal = FullDirichletCalibrator()
-        self.cal.fit(S, y)
-        predictions = self.cal.predict_proba(S).argmax(axis=1)
-        acc = accuracy_score(y, predictions)
-        self.assertGreater(acc, 0.98, "accuracy must be superior to 99 percent")
+        for S, y in (get_simple_binary_example(),
+                     get_simple_ternary_example()):
+            self.cal = FullDirichletCalibrator()
+            self.cal.fit(S, y)
+            predictions = self.cal.predict_proba(S).argmax(axis=1)
+            acc = accuracy_score(y, predictions)
+            self.assertGreater(acc, 0.98,
+                               "accuracy must be superior to 99 percent")
 
     def test_extreme_values(self):
         S, y = get_extreme_binary_example()
@@ -33,26 +27,29 @@ class TestFullDirichlet(unittest.TestCase):
         self.cal.fit(S, y)
         predictions = self.cal.predict_proba(S).argmax(axis=1)
         acc = accuracy_score(y, predictions)
-        self.assertGreater(acc, 0.99, "accuracy must be superior to 99 percent")
-
+        self.assertGreater(acc, 0.99,
+                           "accuracy must be superior to 99 percent")
 
     def test_lambda(self):
         S, y = get_simple_ternary_example()
-        l2_odir = [1e-2]
-        cal = FullDirichletCalibrator(reg_lambda_list=l2_odir,
-                                      reg_mu_list=l2_odir, reg_norm=False)
+        l2_odir = 1e-2
+        cal = FullDirichletCalibrator(reg_lambda=l2_odir,
+                                      reg_mu=l2_odir, reg_norm=False)
         cal.fit(S, y)
         predictions = cal.predict_proba(S).argmax(axis=1)
         acc = accuracy_score(y, predictions)
-        self.assertGreater(acc, 0.98, "accuracy must be superior to 99 percent")
+        self.assertGreater(acc, 0.98,
+                           "accuracy must be superior to 99 percent")
 
-        l2_odir = [1e2]
-        cal = FullDirichletCalibrator(reg_lambda_list=l2_odir,
-                                      reg_mu_list=l2_odir, reg_norm=False)
+        l2_odir = 1e2
+        cal = FullDirichletCalibrator(reg_lambda=l2_odir,
+                                      reg_mu=l2_odir, reg_norm=False)
         cal.fit(S, y)
         predictions = cal.predict_proba(S).argmax(axis=1)
         acc = accuracy_score(y, predictions)
-        self.assertLess(acc, 0.6, "accuracy must be smaller than 60 percent")
+        self.assertLess(acc, 0.6,
+                        "accuracy must be smaller than 60 percent")
+
 
 def main():
     unittest.main()
